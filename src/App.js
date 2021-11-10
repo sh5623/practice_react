51daf63d0e34d3553845be29593a3dcf34bf5071
SeungHo Lee <sh5623789@naver.com>
2021-11-10 14:39:03 +0900

    make todoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodo("");
+    setTodos((currentArray) => [todo, ...todos]);
+  };
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>My To Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {todos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
